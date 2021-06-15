a69ad0ff2165e3b8e018fb5220ec878ba563c75f
Sai Sandeep <korrapatisaisandeep@hotmail.com>
2021-06-15 17:05:58 +0530

    yml modifications

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-require("./db/mongoose");
 require("dotenv").config();
+require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
