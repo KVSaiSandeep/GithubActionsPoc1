8e08245f2fe7f20a231f82c5b024c0d936fa824c
Sai Sandeep <korrapatisaisandeep@hotmail.com>
2021-06-16 13:57:19 +0530

    feat(Sematic Releases): Sematic Release and Versioning Feature added

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/KVSaiSandeep/GithubActionsPoc1",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ]
+};
